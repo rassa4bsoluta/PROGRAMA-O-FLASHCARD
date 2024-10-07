6f466117c5ce9e31ed7a89d3d7e0043c617efac1
rassa4bsoluta <163851395+rassa4bsoluta@users.noreply.github.com>
2024-10-07 08:02:28 -0300

    Add files via upload
    Maria Luiza 25
    Daniela Parra 07
    Thays Waligura 37

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Qual a diferença entre compilar e interpretar um programa?',
+    'Compilar transforma o código inteiro em um arquivo executável de uma vez. Interpretar executa o código linha por linha'
+)
+    
+criaCartao(
+    'Programação',
+    'O que é orientação a objetos?',
+    'É um paradigma que organiza o código em objetos com atributos e métodos.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'É uma sequência de instruções para resolver um problema'
+)
+
+criaCartao(
+    'Programação',
+    'Quais as principais estruturas de dados?',
+    'Vetores, listas ligadas, pilhas, filas, árvores e grafos são algumas das principais.'
+)
